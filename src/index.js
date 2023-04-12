02fc35a7b8320228643f1909300fc177bfb2aa2b
Abbas Sarwar <abbassarwar100@gmail.com>
2023-04-12 13:25:52 +0500

    User created for game

--- src/index.js
+++ src/index.js
@@ -19,5 +19,21 @@ async function Game() {
     return Unique;
   };
   const codee = await UniqueCode();
+
+  const newUser = (User, score) => {
+    const user = fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${codee}/scores/`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: User,
+        score,
+      }),
+    }).then((res) => res.json())
+      .then((data) => data);
+    console.log(user);
+  };
+  await newUser('Abbas', 100);
 }
 Game();
